d6ebf5ae2ec2e3d9828a31b0b4842a657805ebfd
xueweiguo <xueweiguo@hotmail.com>
2017-12-05 23:34:50 +0800

    bug fix

--- NOIP201711/App/App/App/app.cpp
+++ NOIP201711/App/App/App/app.cpp
@@ -13,9 +13,9 @@ int main()
 	//freopen("phalanx.out", "w", stdout);
 	int t = 0;;
 	scanf("%d", &t);
-	bool ok = true;
-	for (int i = 1; ok && i <= t; ++i)
+	for (int i = 1; i <= t; ++i)
 	{
+		bool ok = true;
 		int rows = 0;
 		char O[16];
 		scanf("%d%s", &rows, O);
@@ -28,21 +28,28 @@ int main()
 		int ncounter = 0;
 		int max_counter = 0;
 		bool count_finished = false;
-		for (int r = 1; ok && r <= rows; ++r) {
+		for (int r = 1; r <= rows; ++r) {
 			char lineType[8];
 			scanf("%s", lineType);
-			if (strcmp(lineType,"F") == 0) {
-				
+			if (strcmp(lineType,"F") == 0) {				
 				scanf("%s%s%s", vars[level], X, Y);
-				for (int l = 0; l < level; ++l) {
-					if (strcmp(vars[l], vars[level]) == 0) {
-						ok = false;
-						break;
+				if (ok) {
+					for (int l = 0; l < level; ++l) {
+						if (strcmp(vars[l], vars[level]) == 0) {
+							ok = false;
+						}
 					}
 				}
+				else {
+					continue;
+				}
+
 				if (!count_finished) {
 					if (strcmp(X, "n") == 0) {
-						count_finished = true;
+						if (strcmp(Y, "n") != 0)
+						{
+							count_finished = true;
+						}
 					}
 					else {
 						if (strcmp(Y, "n") == 0) {
@@ -54,7 +61,7 @@ int main()
 						else {
 							int x = atoi(X);
 							int y = atoi(Y);
-							if (x > 0 && y > 0 && x >= y) {
+							if (x > 0 && y > 0 && x > y) {
 								count_finished = true;
 							}
 						}
@@ -63,13 +70,18 @@ int main()
 				level++;
 			}
 			else if (strcmp(lineType,"E") == 0) {
+				if (!ok) continue;
 				level--;
 				if (level == 0) {
 					ncounter = 0;
+					count_finished = false;
+				}
+				else if (level < 0) {
+					ok = false;
 				}
 			}
 			else {
-				//ok = false;
+				ok = false;
 				//break;
 			}
 		}
